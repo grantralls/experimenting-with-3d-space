54a1ed8f2dec2eb240b9e5f840d6a996cb8f04f7
gralls <grall001@odu.edu>
2020-12-22 14:12:15 -0500

    enabling ability to control camera orbitally by click and drag.

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+const controls = new THREE.OrbitControls( camera, renderer.domElement );
+
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
 const cube = new THREE.Mesh(geometry, material);
@@ -12,10 +14,21 @@ scene.add(cube);
 
 camera.position.z = 5;
 
+function getMouseXMovement() {
+    let initialX;
+
+    document.addEventListener("click", (event) => {
+        console.log(event);
+    });
+}
+
+// getMouseXMovement();
+
 function animate() {
+
     requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
 
     renderer.render(scene, camera);
 }
